af9c9453ed321ccdc0c830952550e5bc22774ef1
华晨 <chanjsq@gmail.com>
2020-03-24 13:36:12 +0800

    首页侧栏和背景音乐

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,44 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="music-btn" @click="playMusic"></div>
+    <audio ref="bgMusic" src="@/assets/audio/bg.mp3"></audio>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld,
+  methods: {
+    playMusic() {
+      this.$refs.bgMusic.play().catch(() => console.log('click to play music'));
+    },
   },
 };
 </script>
+
+<style scoped>
+.home {
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  background: #fbec71 url(~@/assets/img/side.jpg) repeat-y 0 0 / 40px;
+  animation: scrolling-side 20s linear infinite both;
+}
+
+.music-btn {
+  position: absolute;
+  top: 25px;
+  right: 23px;
+  width: 22px;
+  height: 52px;
+  background: url(~@/assets/img/music-btn.png) no-repeat center / contain;
+}
+
+@keyframes scrolling-side {
+  from {
+    background-position-y: 0;
+  }
+  to {
+    background-position-y: -1352px;
+  }
+}
+</style>
