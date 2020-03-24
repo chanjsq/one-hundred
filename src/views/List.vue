abca6e40c2ab3d0e393d1ed507427e3efd35de1c
华晨 <chanjsq@gmail.com>
2020-03-24 23:26:27 +0800

    存档

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="list">
     <div class="top"></div>
   </div>
 </template>
@@ -11,6 +11,11 @@ export default {
 </script>
 
 <style scoped>
+.list {
+  height: 100vh;
+  background: #f0f0f0;
+}
+
 .top {
   height: 42px;
   background: url(~@/assets/img/select-top.png) no-repeat 0 0 / cover;
