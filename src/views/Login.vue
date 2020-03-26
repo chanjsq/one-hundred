43a9634da02ad68a8ec1fd702a0fc1ad1499a4f1
华晨 <chanjsq@gmail.com>
2020-03-26 17:05:01 +0800

    隐藏错误提示

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -25,6 +25,7 @@ export default {
     onSubmit() {
       if (this.name) {
         this.$emit('login', this.name);
+        this.showError = false;
         this.$refs.clickMusic.play().then(() => {
           this.$router.replace('/list');
         });
